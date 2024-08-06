f61fe7d8c086a56a93313c4f94072d59be035e97
gabr <arkadiuszgabrys@gmail.com>
2024-08-06 15:02:55 +0200

    simiple stories cache and removed active clients
    Added simple stories cache as we can assume that the story did not
    changed much in a span of few seconds.  We still always query the
    list of the best stories.
    
    Also more tests revealed unstable nature of using many clients.  Using
    two clients does not yield the fastest results but these are the most
    stable - typically around 1-2 seconds.  Where using 50-100 clients
    typically produce results under 1 second but sometimes will stale the
    connection for half a minute or longer.  I prefer more stable but
    slightly slower solution as with the cache it is now fast enough.

--- Api/HackerNewsService.cs
+++ Api/HackerNewsService.cs
@@ -1,26 +1,22 @@
 using System.Diagnostics;
+using System.Collections.Concurrent;
 
 namespace Api;
 
 public class HackerNewsService : BackgroundService, IDisposable  {
-    /* NOTE: An arbitrary number of clients for faster queries
-     *
-     * Hacker News seems to limit how fast it can accept new connections from
-     * given address.  The more connections we have the more requests can we
-     * pipeline and having like 100 clients connected allows us to query 100
-     * stories almost in an instant (200 ms).  But waiting for those 100
-     * clients to connect for the first time takes almost a minute.
-     *
-     * Actually having just two connections seems to be optimal both or first
-     * and subsequent queries allowing us to have all the responses in around
-     * two to three seconds.  So start with two and gradually increase the
-     * connected clients over time.
+    private static readonly TimeSpan FETCH_DELAY = TimeSpan.FromSeconds(1.5);
+    private static readonly TimeSpan CACHE_EXPIRATION = TimeSpan.FromSeconds(5);
+
+    /* NOTE: It seems to work the fastest with just two clients>
+     *  Way faster than with just one and the extensive tests
+     *  with more clients provided unstable results.
      */
-    private readonly IHackerNewsClient[] _clients = new IHackerNewsClient[50];
-    private int _activeClients = 2;
+    private readonly IHackerNewsClient[] _clients = new IHackerNewsClient[2];
     private Task<HackerNewsStory>[] _tasks = new Task<HackerNewsStory>[HackerNewsClient.MAX_IDS_COUNT];
     private TaskCompletionSource<bool> _fetchedFirstStories = new TaskCompletionSource<bool>();
     private volatile HackerNewsStory[] _stories = Array.Empty<HackerNewsStory>();
+    private ConcurrentDictionary<int, CachedHackerNewsStory> _storiesCache
+        = new ConcurrentDictionary<int, CachedHackerNewsStory>(-1, HackerNewsClient.MAX_IDS_COUNT*2);
     private readonly ILogger<HackerNewsService> _logger;
     private bool _disposed = false;
 
@@ -46,11 +42,11 @@ public class HackerNewsService : BackgroundService, IDisposable  {
                 stopWatch.Start();
                 await FetchNewData(ct);
                 stopWatch.Stop();
-                _logger.LogDebug($"{nameof(HackerNewsService)}: {DateTime.UtcNow.ToString("o")} fetched data - elapsed: {stopWatch.Elapsed} with {_activeClients}/{_clients.Length} clients");
+                _logger.LogDebug($"{nameof(HackerNewsService)}: {DateTime.UtcNow.ToString("o")} fetched data - elapsed: {stopWatch.Elapsed}");
                 stopWatch.Reset();
                 // double check before the dealy as the fetching might have taken a while
                 if (ct.IsCancellationRequested) break;
-                await Task.Delay(TimeSpan.FromSeconds(1.5), ct);
+                await Task.Delay(FETCH_DELAY, ct);
             }
             _logger.LogInformation($"{nameof(HackerNewsService)} main loop stopped");
         } catch (Exception ex) {
@@ -65,24 +61,52 @@ public class HackerNewsService : BackgroundService, IDisposable  {
 
     private async Task FetchNewData(CancellationToken ct) {
         try {
+            // always get entire list of best stories
             var bestStoriesIds = await _clients[0].GetNBestStoriesIdsAsync(HackerNewsClient.MAX_IDS_COUNT, ct);
             var stories = new HackerNewsStory[bestStoriesIds.Length];
             // kick start all the requests
-            Parallel.For(0, stories.Length, (i, _) => _tasks[i] = _clients[i%_activeClients].GetStoryByIdAsync(bestStoriesIds[i], ct));
-            // use more connections next time
-            if (_activeClients != _clients.Length) {
-                _activeClients += 2;
-                if (_activeClients > _clients.Length)
-                    _activeClients = _clients.Length;
-            }
-            // wait for the responses and parse
-            await Parallel.ForAsync(0, stories.Length, ct, async (i, _) => stories[i] = await _tasks[i]);
+            var fetchTime = DateTime.UtcNow;
+            Parallel.For(0, stories.Length, (i, _) => {
+                var id = bestStoriesIds[i];
+                _tasks[i] = GetStoryFromCache(id, fetchTime) ??
+                    // if not in cache then fetch it from the API
+                    _clients[i%_clients.Length].GetStoryByIdAsync(id, ct);
+            });
+            // wait for the responses and update cache
+            await Parallel.ForAsync(0, stories.Length, ct, async (i, _) => {
+                stories[i] = await _tasks[i];
+                AddStoryCache(stories[i], fetchTime);
+            });
+            // replace old collection with new data
             _stories = stories;
+            // and inform waiting clients that data is available
             if (!_fetchedFirstStories.Task.IsCompleted)
                 _fetchedFirstStories.SetResult(true);
         } catch (Exception ex) {
             _logger.LogError($"Fetching data exception: '{ex.ToString()}'", ex);
         }
+    }
+
+    private Task<HackerNewsStory>? GetStoryFromCache(int id, DateTime fetchTime) {
+        if (!_storiesCache.ContainsKey(id)) return null;
+        var cached = _storiesCache[id];
+        if ((fetchTime - cached.fetchTime) > CACHE_EXPIRATION) {
+            // intentionally ignoring return value as it should be false only
+            // if value was not found which we know for sure is there
+            _ = _storiesCache.TryRemove(id, out _); // remove if expired
+            return null;
+        }
+        return Task.FromResult(cached.story);
+    }
+
+    private void AddStoryCache(HackerNewsStory story, DateTime fetchTime) {
+        if (_storiesCache.ContainsKey(story.Id)) return;
+        // intentionally ignoring the return value as it fails to add only if
+        // the key already has assigned value which we know won't be the case
+        _ = _storiesCache.TryAdd(story.Id, new CachedHackerNewsStory() {
+            fetchTime = fetchTime,
+            story = story,
+        });
     }
 
     public override void Dispose() {
@@ -92,6 +116,11 @@ public class HackerNewsService : BackgroundService, IDisposable  {
         }
         _disposed = true;
         base.Dispose();
+    }
+
+    private class CachedHackerNewsStory {
+        public required DateTime fetchTime;
+        public required HackerNewsStory story;
     }
 }
 
